11bea7e25823143ee8bb527ac575d027f8993212
Iulia Anca <iulia.anca@evozon.com>
2024-11-18 19:35:48 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AncaIuliaAnaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
